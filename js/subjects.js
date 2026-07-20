440ef92e5a174a535dd307f719ab3336f311f64f
eswar585 <eswaravuthu04@gmail.com>
2026-07-20 18:34:31 +0530

    Update subjects.js

--- js/subjects.js
+++ js/subjects.js
@@ -10,12 +10,7 @@ const subjects = [
         code: "25CY206",
         name: "Statistical Foundation for Computing and Data Science",
         icon: "📊",
-
-        syllabus: "pdfs/syllabus/25CY206.pdf",
-
-        notesAvailable: false,
-
-        notes: []
+        syllabus: "pdfs/syllabus/25CY206.pdf"
     },
 
     {
@@ -23,29 +18,7 @@ const subjects = [
         code: "25AM201",
         name: "Data Structures",
         icon: "💻",
-
-        syllabus: "pdfs/syllabus/25AM201.pdf",
-
-        notesAvailable: true,
-
-        notes: [
-
-            {
-                title: "Lecture 01 - Data Structures",
-                file: "pdfs/notes/25AM201/Lec_01(Data_structures).pdf"
-            },
-
-            {
-                title: "Lecture 02 - Linear Search",
-                file: "pdfs/notes/25AM201/Lec_02(Linear_search).pdf"
-            },
-
-            {
-                title: "Lecture 03 - Binary Search",
-                file: "pdfs/notes/25AM201/Lec_03(Binary_search).pdf"
-            }
-
-        ]
+        syllabus: "pdfs/syllabus/25AM201.pdf"
     },
 
     {
@@ -53,12 +26,7 @@ const subjects = [
         code: "25AM202",
         name: "Design Thinking and Engineering Orientation",
         icon: "🎨",
-
-        syllabus: "pdfs/syllabus/25AM202.pdf",
-
-        notesAvailable: false,
-
-        notes: []
+        syllabus: "pdfs/syllabus/25AM202.pdf"
     },
 
     {
@@ -66,12 +34,7 @@ const subjects = [
         code: "25AM203",
         name: "Artificial Intelligence",
         icon: "🤖",
-
-        syllabus: "pdfs/syllabus/25AM203.pdf",
-
-        notesAvailable: false,
-
-        notes: []
+        syllabus: "pdfs/syllabus/25AM203.pdf"
     },
 
     {
@@ -79,12 +42,7 @@ const subjects = [
         code: "25CY205",
         name: "Database Management Systems",
         icon: "🗄️",
-
-        syllabus: "pdfs/syllabus/25CY205.pdf",
-
-        notesAvailable: false,
-
-        notes: []
+        syllabus: "pdfs/syllabus/25CY205.pdf"
     },
 
     {
@@ -92,12 +50,7 @@ const subjects = [
         code: "25AM206",
         name: "Object Oriented Programming",
         icon: "☕",
-
-        syllabus: "pdfs/syllabus/25AM206.pdf",
-
-        notesAvailable: false,
-
-        notes: []
+        syllabus: "pdfs/syllabus/25AM206.pdf"
     },
 
     {
@@ -105,12 +58,7 @@ const subjects = [
         code: "25MT205",
         name: "Discrete Mathematical Structures",
         icon: "📐",
-
-        syllabus: "pdfs/syllabus/25MT205.pdf",
-
-        notesAvailable: false,
-
-        notes: []
+        syllabus: "pdfs/syllabus/25MT205.pdf"
     }
 
 ];
